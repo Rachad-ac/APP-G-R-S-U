1ca769529a464a5a912db2a1fd798fa2b48d14e4
Rachad <rachad@gmail.com>
2025-09-16 19:24:35 +0000

    implementation models  user et role + getstion auth et droit d'acces

--- database/migrations/2025_09_16_180759_create_users_table.php
+++ database/migrations/2025_09_16_180759_create_users_table.php
@@ -13,13 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom' , 100);
+            $table->string('prenom', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
+            $table->unsignedBigInteger('id_role');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('id_role')->references('id_role')->on('roles')->onDelete('cascade');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
